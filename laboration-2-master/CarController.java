7211c1b3a12323ce1bf37bf14488581b54ab04e4
ALehnberg <guslehnal@student.gu.se>
2020-02-17 15:33:00 +0100

    ye

--- laboration-2-master/CarController.java
+++ laboration-2-master/CarController.java
@@ -13,6 +13,11 @@ import java.util.ArrayList;
 public class CarController {
     // member fields:
 
+
+
+    //incendium
+
+
     // The delay (ms) corresponds to 20 updates a sec (hz)
     private final int delay = 50;
     // The timer is started with an listener (see below) that executes the statements
